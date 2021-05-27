8d79fe0f0bc46223b640c221fdf858318f224e86
unknown <engineeringc122@gmail.com>
2021-05-27 19:10:17 +0300

    done

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+ console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,7 +20,11 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  if (n % 2===1){
+    return true ;
+    else
+     return false 
+  }
 }
 
 /**
@@ -33,7 +37,7 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+ return parseInt(n/2)
 }
 
 /**
@@ -47,7 +51,12 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  if (n % 2===1){
+    return  n^2 ;
+    else
+     return n*2;
+  }
+  
 }
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
